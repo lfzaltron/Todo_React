6a6676422d8a672c33a8d0346dcfc7386833cfc3
Luis Fernando Zaltron <lfzaltron@gmail.com>
2022-07-07 11:29:57 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header"
 
 export function App() {
 
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header />
+      <h1>Hello world!</h1>
+    </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Icon } from "./Icon";
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-center bg-gray-700 min-h-[200px]">
+      <Icon />
+    </header>
+  );
+}
